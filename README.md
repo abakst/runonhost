d801be875acf06cb858a2d5bd705afd2d301bd10
Alexander Bakst <alexander.bakst@alum.mit.edu>
2012-05-10 13:45:22 -0700

    add support for a map file. write a readme

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 runonhost
 =========
 
-Attempts to determine if a command is to be executed on a filesystem that lives on a remote host. If so, runs the command on the remote host via ssh.
+Attempts to determine if a command is to be executed on a filesystem
+that lives on a remote host. If so, runs the command on the remote
+host via ssh.
+
+This is done via two mechanisms. The user may specify a file (by
+default $HOME/.sharemap) that maps directories on the local file
+system to directories on the remote filesystem. The format of this
+file is:
+
+/path/on/local/fs/ host:/path/on/host/fs
+
+The script may also check the output of the `mount' command to
+discover any file systems mounted via sshfs.
+
+Usage:
+$ runonhost.sh [-h] [-m] [-f map_file] cmd
+
+  -h             Display the above usage.
+  
+  -m             Force runonhost.sh to use the output of mount.
+  
+  -f map_file    Use map_file instead of the default ($HOME/.sharemap) to
+                 read mappings between local and remote directories.
+
--- runonhost.sh
+++ runonhost.sh
@@ -1,21 +1,77 @@
 #!/bin/bash -f
 
+# Check the output of mount for sshfs mounted directories, and do The
+# Right Thing
+function doMount () {
+  mount | awk -v cwd=$CWD -v cmd="${CMD}" -v cdcmd="$CDCMD" '
+  /.*@.*:.* on / { 
+    if ( cwd ~ $3 ) {
+      sep=index($1, ":")
+      host=substr($1,0,sep-1)
+      newpath=substr($1,sep+1,255)
+  
+      gsub($3, newpath, cdcmd)
+  
+      cmd="ssh " host " " cdcmd
+    }
+  }
+  END { 
+    print cmd
+  }' | bash
+}
+
+# Check the contents of 
+function doMap() {
+  awk -v cwd=$CWD -v cmd="${CMD}" -v cdcmd="$CDCMD" '
+  /^.* .*:.*/ {
+    if ( cwd ~ $1 ) {
+      sep=index($2, ":")
+      host=substr($2,0,sep-1)
+      newpath=substr($2,sep+1,255)
+     
+      gsub($1, newpath, cdcmd)
+      cmd="ssh " host " " cdcmd
+    }
+  }
+  END {
+    print cmd
+  }' $MAP_FILE | bash
+}
+
+function usage () {
+    echo "Usage: `basename $0` [-h] [-m] [-f mapfile] cmd ..."
+}
+
+while getopts ":hmf:" opt; do
+  case $opt in
+    m) 
+       FORCE_MOUNT=1
+       ;;
+    f)
+       MAP_FILE=${OPTARG}
+       ;;
+    h)
+       usage
+       exit 0
+       ;;
+    \?)
+       usage
+       exit 1
+       ;;
+  esac
+  shift $((OPTIND-1))
+done      
+
 CWD=$(pwd -P)
 CMD="$@"
-CDCMD="\'cd $CWD; $@\'"
+CDCMD="'cd $CWD; $@'"
 
-mount | awk -v cwd=$CWD -v cmd="${CMD}" -v cdcmd="$CDCMD" '
-/.*@.*:.* on / { 
-  if ( cwd ~ $3 ) {
-    sep=index($1, ":")
-    host=substr($1,0,sep-1)
-    newpath=substr($1,sep+1,255)
+if [[ -z $MAP_FILE ]]; then
+  MAP_FILE="$HOME/.sharemap"
+fi
 
-    gsub($3, newpath, cdcmd)
-
-    cmd="ssh " host " " cdcmd
-  }
-}
-END { 
-  print cmd
-}' | bash
+if [[ ! -z $FORCE_MOUNT || ! -e $MAP_FILE ]]; then
+    doMount
+else
+    doMap
+fi
